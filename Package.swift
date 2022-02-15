455f49d52afa86ab44e9c29dfb4799fef017b3b1
fwcd <fwcdmail@gmail.com>
2022-02-15 18:56:08 +0100

    Add messagepack library

--- Package.swift
+++ Package.swift
@@ -15,6 +15,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/vapor/websocket-kit.git", from: "2.3.0"),
+        .package(url: "https://github.com/Flight-School/MessagePack.git", from: "1.2.4"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
