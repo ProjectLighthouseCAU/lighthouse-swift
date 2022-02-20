162c10819bd7eda6c318fc56240dc51b5118a3e4
fwcd <fwcdmail@gmail.com>
2022-02-21 00:04:30 +0100

    Render displays to test server's web interface

--- Public/scripts.js
+++ Public/scripts.js
@@ -1,5 +1,34 @@
+const lighthouseRows = 14;
+const lighthouseCols = 28;
+const xScale = 14;
+const yScale = 2 * xScale;
+
 let connection = null;
 let auth = null;
+let display = null;
+
+function updateDisplay(rgb) {
+    const ctx = display.getContext("2d");
+
+    for (let i = 0; i < (rgb.length / 3); i++) {
+        const r = rgb[3 * i];
+        const g = rgb[3 * i + 1];
+        const b = rgb[3 * i + 2];
+
+        const y = Math.floor(i / lighthouseCols);
+        const x = i % lighthouseCols;
+
+        ctx.fillStyle = `rgb(${r},${g},${b})`;
+        ctx.fillRect(x * xScale, y * yScale, xScale, yScale);
+    }
+}
+
+function setUpDisplay() {
+    display = document.getElementById("display");
+    display.width = xScale * lighthouseCols;
+    display.height = yScale * lighthouseRows;
+    updateDisplay(new Uint8Array(3 * lighthouseRows * lighthouseCols));
+}
 
 function setUpConnection() {
     connection = new WebSocket(`${location.origin.replace(/^http/, "ws")}/websocket`);
@@ -14,8 +43,9 @@ function setUpConnection() {
             const message = MessagePack.decode(new Uint8Array(event.data));
 
             if (message.PAYL instanceof Uint8Array) {
-                const display = document.getElementById("display");
-                display.innerText = message.PAYL.map(x => `${x}`).join(", ");
+                updateDisplay(message.PAYL);
+            } else {
+                console.log(`Something else: ${message.PAYL instanceof Uint8Array}`);
             }
         } catch (e) {
             console.log(`Error while decoding message from WebSocket: ${e}`);
@@ -60,6 +90,7 @@ function setUpFormListener() {
 }
 
 window.addEventListener("load", () => {
+    setUpDisplay();
     setUpConnection();
     setUpFormListener();
 });
