0656df30c46e1645627dee81768eedf23f950ba4
fwcd <fwcdmail@gmail.com>
2022-08-15 16:54:02 +0200

    Add DocC documentation

--- Package.swift
+++ Package.swift
@@ -23,6 +23,7 @@ let package = Package(
         .package(url: "https://github.com/Flight-School/MessagePack.git", from: "1.2.4"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.4.2"),
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.3"),
+        .package(url: "https://github.com/apple/swift-docc-plugin.git", from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
