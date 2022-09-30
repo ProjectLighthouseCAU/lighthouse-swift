21b0dba4b379615869b554a717e83ae88b0b6625
fwcd <fwcdmail@gmail.com>
2022-09-30 21:49:54 +0200

    Update demo and example

--- Sources/LighthouseDemo/LighthouseDemo.swift
+++ Sources/LighthouseDemo/LighthouseDemo.swift
@@ -26,22 +26,24 @@ struct LighthouseDemo: ParsableCommand {
         let auth = Authentication(username: username, token: token)
         let lh = Lighthouse(authentication: auth, url: url)
 
-        // Handle incoming input events
-        lh.onInput { input in
-            log.info("Got input \(input)")
-        }
-
         // Connect to the lighthouse server
         try await lh.connect()
         log.info("Connected to the lighthouse")
 
-        // Request a stream of events
-        try await lh.requestStream()
+        // Handle incoming input events
+        Task {
+            let stream = try await lh.streamModel()
+            for await message in stream {
+                if case let .inputEvent(input) = message.payload {
+                    log.info("Got input \(input)")
+                }
+            }
+        }
 
         // Repeatedly send colored frames to the lighthouse
         while true {
             log.info("Sending frame")
-            try await lh.send(frame: Frame(fill: .random()))
+            try await lh.putModel(frame: Frame(fill: .random()))
             try await Task.sleep(nanoseconds: 1_000_000_000)
         }
     }
